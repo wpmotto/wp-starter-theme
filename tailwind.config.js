40b06145bc2f5a555d9c5fdfdfd79d0d22408822
Greg Hunt <greg@motto.ca>
2021-10-26 15:25:58 -0400

    Integrated TW with theme.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,40 @@
+const _ = require("lodash");
+const theme = require('./theme.json');
+const tailpress = require("@jeffreyvr/tailwindcss-tailpress");
+
 module.exports = {
+  mode: 'jit',
   purge: {
     content: [
       './app/**/*.php',
       './resources/**/*.{php,vue,js}',
+      './safelist.txt'
     ],
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      colors: {},
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '0rem'
+      },
     },
+    extend: {
+      colors: tailpress.colorMapper(tailpress.theme('settings.color.palette', theme))
+    },
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': tailpress.theme('settings.layout.wideSize', theme)
+    }
   },
   variants: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [
+    require('@tailwindcss/typography'),
+    tailpress.tailwind
+  ],
 };
